755e171ee3f06a71e90bbea061cfcaa48b04cc81
segev.dagan <segev.dagan@git.ilrd.com>
2018-09-11 22:15:28 +0300

    initial commit

--- sesh.c
+++ sesh.c
@@ -15,8 +15,6 @@
 #include <unistd.h>    // fork
 #include <sys/wait.h>  // wait
 
-#define OUT 0 
-#define IN  1 
 #define MAX_ARGS 256
 #define UNUSED(x) ((void)(x))
 
@@ -165,32 +163,3 @@ static void ExitCommand(void)
 {
     g_exit_flag = FALSE;
 }
-
-// static unsigned int CountArguments(char *str) 
-// { 
-//     int state = OUT; 
-//     unsigned wc = 0;  // word count 
-  
-//     // Scan all characters one by one 
-//     while (*str) 
-//     { 
-//         // If next character is a separator, set the  
-//         // state as OUT 
-//         if (*str == ' ' || *str == '\n' || *str == '\t') 
-//             state = OUT; 
-  
-//         // If next character is not a word separator and  
-//         // state is OUT, then set the state as IN and  
-//         // increment word count 
-//         else if (state == OUT) 
-//         { 
-//             state = IN; 
-//             ++wc; 
-//         } 
-  
-//         // Move to next character 
-//         ++str; 
-//     } 
-  
-//     return wc; 
-// } 
